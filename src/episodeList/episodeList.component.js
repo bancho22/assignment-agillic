837c0e9dd4b1455347243a808c500d9fba72a8e9
bancho_p <banchopetrov@gmail.com>
2018-09-05 12:44:44 +0200

    styled episode list component

--- src/episodeList/episodeList.component.js
+++ src/episodeList/episodeList.component.js
@@ -4,16 +4,24 @@ import {connect} from 'react-redux'
 
 const formatNumber = number => (number < 10 ? `0${number}` : `${number}`)
 
-const generateName = ({season, number, name}) => `S${formatNumber(season)}E${formatNumber(number)} ${name}`
+const generateSeeXyyString = ({season, number}) => `S${formatNumber(season)}E${formatNumber(number)}`
 
 const EpisodeList = ({episodes}) => (
-  <div className='show-overview'>
-    {episodes.map(({id, url, name, season, number, image}) => (
-      <li key={id}>
-        {image && image.medium && <img src={image.medium} alt='episode-poster' />}
-        {generateName({season, number, name})}
-      </li>
-    ))}
+  <div>
+    <h2>Episodes</h2>
+    <div className='episode-list'>
+      {episodes.map(({id, url, name, season, number, image}) => (
+        <div className='episode-container'>
+          <div key={id} className='episode-thumb'>
+            {image && image.medium && <img src={image.medium} alt='episode-poster' />}
+            <div className='episode-meta'>
+              <span className='episode-seexyy'>{generateSeeXyyString({season, number})}</span>
+              <span className='episode-name'>{name}</span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   </div>
 )
 
